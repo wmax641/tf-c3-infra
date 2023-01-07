d1b4816085c336baebc325f90a369aa08ac5cd0e
wmax641 <no-reply@wmax641.website>
2023-01-07 18:33:05 +0900

    sg 1337

--- dns_internal.tf
+++ dns_internal.tf
@@ -4,6 +4,7 @@ resource "aws_route53_zone" "internal-wmax641-website" {
   vpc {
     vpc_id = aws_vpc.main.id
   }
+  tags = merge({ "Name" = "internal.wmax641.website" }, var.common_tags)
 }
 
 resource "aws_route53_record" "proxy-internal-wmax641-website" {
